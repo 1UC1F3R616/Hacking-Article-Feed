93c3711802f21c3c19ea07f1822672fadd8abe7a
1UC1F3R616 <kushchoudhary8@gmail.com>
2020-12-23 23:18:17 +0530

    pentester blogs added

--- bot.py
+++ bot.py
@@ -8,13 +8,17 @@ API_KEY = os.getenv('API_KEY')
 # scraper import
 from scrapes.bug_bytes import result as bug_bytes_result
 from scrapes.portswigger_research import result as portswigger_research_result
+from scrapes.pentester_land import result as pentester_land_result
 
 # scraper run
 def run_bot():
     bug_bytes_result(API_KEY=API_KEY, CHAT_ID=CHAT_ID)
     portswigger_research_result(API_KEY=API_KEY, CHAT_ID=CHAT_ID)
+    pentester_land_result(API_KEY=API_KEY, CHAT_ID=CHAT_ID)
+
 
 if __name__ == '__main__':
     while True:
         run_bot()
-        sleep(1*60*60*6) # every 6 hours
+        exit(0)
+        #sleep(1*60*60*6) # every 6 hours
